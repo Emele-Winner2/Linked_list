8e57caba2eba049b6eebca40600dee23b7a3e720
Emele-Winner2 <winneremele6@gmail.com>
2026-05-14 02:19:34 +0100

    Implemented Initialize linked_list

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,13 +1,20 @@
 #include "./include/linked_list.h"
 #include "stdio.h"
+#include "stdlib.h"
 typedef struct
 {
     int value;
     linked_list *next;
 } linked_list;
 extern linked_list *Head;
+extern linked_list *checker;
 void Initialize_list()
 {
+    Head = malloc(sizeof(linked_list));
     Head->next = NULL;
     Head->value = NULL;
+}
+void Insert_node(char *name)
+{
+    linked_list name;
 }
